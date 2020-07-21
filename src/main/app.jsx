62b9fa96e524567fe148672c0872cb0c85ac85dd
Luizcampos84 <luiz84campos@gmail.com>
2020-07-21 19:29:00 +1000

    Primeiro commit

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.mim.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Test</h1>
+    </div>
+)
